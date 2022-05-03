f9bd1c4476604ab92887c8db22977b5efd0348dc
park-siwan <pswan3168@gmail.com>
2022-05-04 00:00:08 +0900

    마스터 삭제 테스트

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//박시완
+//마스터 삭제 테스트
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
