3885ac1f9a8684e232e00d5ead9fb1cb5b48094a
park-siwan <pswan3168@gmail.com>
2022-05-07 20:58:35 +0900

    react18에 맞게 createRoot로 변경

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 import { Font } from '@react-pdf/renderer';
+import { createRoot } from 'react-dom/client';
+
 Font.register({
   family: 'Spoqa',
   fonts: [
@@ -20,15 +22,16 @@ Font.register({
     },
   ],
 });
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <App tab='home' />
     </RecoilRoot>
-  </React.StrictMode>,
-
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
